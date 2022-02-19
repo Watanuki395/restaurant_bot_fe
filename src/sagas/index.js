8e5a201bdc32c939028e25d48e1924435435b431
Gerardo Salas Montoya <gesalas@kyndryl.com>
2022-02-19 09:58:41 -0600

    adding hotfix

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,7 +4,6 @@ import loginSaga from './loginSaga';
 import registerSaga from './registerSaga';
 import forgotpassSaga from './forgotpassSaga';
 
-
 export default function* rootSaga() {
   yield all([
     fork(loginSaga),
