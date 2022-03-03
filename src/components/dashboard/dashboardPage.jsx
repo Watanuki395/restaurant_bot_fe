2c070385e47075926cbd1b227b10b7310067c30a
Alex Cascante <alexcascante.689@yahoo.com>
2022-03-02 18:54:37 -0600

    Table en Dashboard

--- src/components/dashboard/dashboardPage.jsx
+++ src/components/dashboard/dashboardPage.jsx
@@ -1,13 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import {Container,
+Col4,
+Col8}
+from "./style";
+import Table from '../common/Table/Table';
 
 function DashboardPage(){
-
     return (
-      <div>
-        Super DashBoard
-      </div>
+      <>
+        <Container>
+          <Col4></Col4>
+          <Col8>
+            <Table data={data}/>
+          </Col8>
+        </Container>
+      </>
     );
-
 }
+
+const data = [
+  {
+    name: "Pan",
+    amount: 1
+  },
+  {
+    name: "Carne",
+    amount: 3
+  },
+  {
+    name: "Leche",
+    amount: 6
+  },
+  {
+    name: "Huevos",
+    amount: 15
+  },
+  {
+    name: "Fruta",
+    amount: 32
+  },
+]
 
 export default DashboardPage;
